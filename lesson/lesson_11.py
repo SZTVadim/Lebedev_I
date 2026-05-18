ad7be6c16a137cadbec7b83fd32178724496e24c
Igor <igor.lebedev@fstravel.com>
2026-05-18 19:41:13 +0300

    Урок 11, первое исполнение

---
+++ lesson/lesson_11.py
@@ -0,0 +1,52 @@
+# ЗАДАНИЕ 1: Класс Book (Книга)
+
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def get_info(self):
+        return f"{self.title}, {self.author}, {self.pages}"
+
+    def is_long(self):
+        if self.pages > 300:
+            return "True"
+        else:
+            return "False"
+
+
+book1 = Book("Одиссея", "Кристофер Нолан", 450)
+book2 = Book("Гарри Поттер", "Роулинг", 766)
+book3 = Book("Золотая рыбка", "А.С. Пушкин", 130)
+print(book1.get_info())
+print(book2.get_info())
+print(book3.get_info())
+print(book1.is_long())
+print(book2.is_long())
+print(book3.is_long())
+
+
+# ЗАДАНИЕ 2: Класс BankAccount (Банковский счёт)
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        return self.balance + amount
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            return self.balance - amount
+        else:
+            return "Недостаточно средств"
+
+    def balances(self):
+        return self.balance
+
+
+score = BankAccount("Igor", 500)
+print(score.balances())
+print(score.deposit(100))
+print(score.withdraw(550))
